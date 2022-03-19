925fd94bad5cc2e13eee8dbd5ee3714a1a0e4bd6
gukasyan-arman <gukasian.a.a@gmail.com>
2022-03-19 19:45:16 +0300

    add correct validation for log in to personal area

--- app/src/main/java/com/example/mygym/screen/personalarea/AuthFragment.kt
+++ app/src/main/java/com/example/mygym/screen/personalarea/AuthFragment.kt
@@ -1,6 +1,9 @@
 package com.example.mygym.screen.personalarea
 
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -22,13 +25,14 @@ class AuthFragment : Fragment() {
     lateinit var binding: FragmentAuthBinding
     private lateinit var auth: FirebaseAuth
     private var verificationId: String = ""
+    private lateinit var phoneNumber: String
+    private val ruRegionNumber = "+7"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentAuthBinding.inflate(inflater, container, false)
-
         return binding.root
     }
 
@@ -37,32 +41,14 @@ class AuthFragment : Fragment() {
 
         auth = FirebaseAuth.getInstance()
 
-        binding.acceptRules.setOnCheckedChangeListener { _, _ ->
-            binding.buttonGetCode.isEnabled = true
-        }
-
         binding.buttonGetCode.setOnClickListener {
-            val phoneNumber = binding.phoneEditText.text.toString().trim()
-            if (phoneNumber.isEmpty() || phoneNumber.length < 10) {
-                Toast.makeText(requireContext(), "Enter correct phone", Toast.LENGTH_SHORT).show()
-                binding.phoneEditText.requestFocus()
-            } else {
-                binding.buttonVerify.isVisible = true
-                binding.otpEditText.isVisible = true
-                binding.buttonGetCode.text = "Получить код повторно"
-//                sendVerificationCode(phoneNumTest)
-                sendVerificationCode(phoneNumber)
-            }
+            phoneNumber = binding.phoneEditText.text.toString().trim()
+            Toast.makeText(requireContext(), phoneNumber, Toast.LENGTH_LONG).show()
+            validatePhone(phoneNumber)
         }
 
         binding.buttonVerify.setOnClickListener {
-            if (binding.otpEditText.text.toString().trim().isEmpty()) {
-                Toast.makeText(requireContext(), "Неверный код", Toast.LENGTH_SHORT).show()
-            } else {
-                verifyCode(binding.otpEditText.text.toString())
-//                verifyCode(smsCodeTest)
-            }
-
+            validateCode(binding.otpEditText.toString().trim())
         }
 
     }
@@ -70,7 +56,7 @@ class AuthFragment : Fragment() {
     private fun sendVerificationCode(phoneNumber: String) {
 
         val options = PhoneAuthOptions.newBuilder(auth)
-            .setPhoneNumber("+7$phoneNumber")       // Phone number to verify
+            .setPhoneNumber(ruRegionNumber + phoneNumber)       // Phone number to verify
             .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
             .setActivity(requireActivity())                 // Activity (for callback binding)
             .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
@@ -111,6 +97,43 @@ class AuthFragment : Fragment() {
                 Toast.makeText(requireContext(), "Login successful", Toast.LENGTH_SHORT).show()
                 Navigation.findNavController(view!!).navigate(R.id.action_global_personalAreaFragment)
             }
+        }
+    }
+
+    private fun validatePhone(phoneNumber: String): Boolean {
+        return if (binding.acceptRules.isChecked) {
+            return if (phoneNumber.isEmpty()) {
+                binding.phoneTextInputLayout.error = "Please, input your phone number"
+                false
+            } else if (phoneNumber.isNotEmpty() && phoneNumber.length < 10) {
+                binding.phoneTextInputLayout.error = "Please, input correct phone number"
+                false
+            } else {
+                binding.buttonVerify.isVisible = true
+                binding.otpTextInputLayout.isVisible = true
+                binding.buttonGetCode.text = "Получить код повторно"
+
+                sendVerificationCode(phoneNumber)
+                binding.phoneTextInputLayout.error = ""
+                true
+            }
+        } else {
+            Toast.makeText(requireContext(), "Примите правила", Toast.LENGTH_SHORT).show()
+            false
+        }
+
+    }
+
+    private fun validateCode(code: String): Boolean {
+        return if (binding.otpEditText.text.toString().trim().isEmpty()) {
+            binding.otpTextInputLayout.error = "Please, input your code"
+            false
+        } else if (code.isNotEmpty() && code.length < 6) {
+            binding.otpTextInputLayout.error = "Please, input correct code"
+            false
+        } else {
+            verifyCode(binding.otpEditText.text.toString())
+            true
         }
     }
 
